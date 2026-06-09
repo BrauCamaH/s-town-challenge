8ef98dac5cf381be126e53b3acdf5fe5bf81db3d
braucamah <braulio.camarenah@gmail.com>
2026-06-08 21:28:44 -0600

    include Category Controller index

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class CategoryController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('categories/index', [
+            'categories' => Category::paginate(5),
+        ]);
+    }
 }
