56c44c2d6129f8eb1c540dc88ff33c917d23596f
braucamah <braulio.camarenah@gmail.com>
2026-06-08 21:28:33 -0600

    include BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\User;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class BookController extends Controller
 {
-    //
+    public function index(): Response
+    {
+        return Inertia::render('books/index', [
+            'books' => Book::with(['categories', 'borrower'])->paginate(5),
+            'users' => User::all(),
+        ]);
+    }
 }
