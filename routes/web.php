4400ffef54b7da0757baaf8d1f0bc0386ee57b88
braucamah <braulio.camarenah@gmail.com>
2026-06-08 21:29:06 -0600

    include books and categories index route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('books', \App\Http\Controllers\BookController::class);
+    Route::resource('categories', \App\Http\Controllers\CategoryController::class);
+
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
+require __DIR__ . '/settings.php';
